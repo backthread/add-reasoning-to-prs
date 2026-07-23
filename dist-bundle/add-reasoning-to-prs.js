6001db1fd155b0250fe1cc7addac75f21b0db45c
Jevgeni Bogatyrjov <35191856+bogatyrjov1@users.noreply.github.com>
2026-07-23 16:06:24 +0300

    Release v1.1.0

--- dist-bundle/add-reasoning-to-prs.js
+++ dist-bundle/add-reasoning-to-prs.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // src/version.ts
-var VERSION = "1.0.0" ? "1.0.0" : readPackageVersion();
+var VERSION = "1.1.0" ? "1.1.0" : readPackageVersion();
 
 // src/stdin.ts
 async function readRawStdin(env = process.env, stdin = process.stdin) {
